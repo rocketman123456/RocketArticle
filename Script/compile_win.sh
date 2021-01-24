c31b63c4874f84bf0b7efdefaaa4f8a656f6944d
rocketman123456 <759094438@qq.com>
2021-01-25 00:07:18 +0800

    update
    '

---
+++ Script/compile_win.sh
@@ -0,0 +1,3 @@
+./Tool/win/glslc.exe Asset/Shaders/shader_base.vert -o Asset/Shaders/Vulkan/vert_base.spv
+./Tool/win/glslc.exe Asset/Shaders/shader_base.frag -o Asset/Shaders/Vulkan/frag_base.spv
+echo "Compile Shader Complete"
