d9db7fa0872fe8f13b0431f38b08dd401940581b
rocketman123456 <759094438@qq.com>
2021-02-12 23:12:39 +0800

    update

--- Rocket/RHI/Vulkan/VulkanGraphicsManager.cpp
+++ Rocket/RHI/Vulkan/VulkanGraphicsManager.cpp
@@ -723,7 +723,7 @@ QueueFamilyIndices VulkanGraphicsManager::findQueueFamilies(VkPhysicalDevice dev
     Vec<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
     vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
 
-    int i = 0;
+    uint32_t i = 0;
     for (const auto& queueFamily : queueFamilies) {
         if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
             indices.graphicsFamily = i;
