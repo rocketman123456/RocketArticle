5071149056040b16ee3e7d0d6eb027b60a8bca93
rocketman123456 <759094438@qq.com>
2021-03-09 19:00:41 +0800

    update linux

--- Script/compile_glm.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p Thirdparty/build/glm
-pushd Thirdparty/build/glm
-rm -rf *
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../../External/`uname -s`/ -DCMAKE_INSTALL_RPATH=../../../External/`uname -s`/ -DGLM_TEST_ENABLE=ON ../../glm
-cmake --build . --config Debug --target install
-popd
--- Script/compile_libcopp.bat
+++
@@ -1,6 +0,0 @@
-mkdir Thirdparty\build\libcopp
-pushd Thirdparty\build\libcopp
-del /S /Q *
-cmake -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DCMAKE_INSTALL_PREFIX=..\..\..\External\Windows\ -DCMAKE_INSTALL_RPATH=..\..\..\External\Windows\ ..\..\libcopp
-cmake --build . --config Debug --target install
-popd
