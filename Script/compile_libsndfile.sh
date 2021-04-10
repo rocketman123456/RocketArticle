562873601dd46263990b8c962434c7463e51c601
Rocketman <759094438@qq.com>
2021-04-10 14:28:25 +0800

    update 3rd party lib

--- Script/compile_libsndfile.sh
+++ Script/compile_libsndfile.sh
@@ -1,6 +1,6 @@
 mkdir -p Thirdparty/build/libsndfile
 pushd Thirdparty/build/libsndfile
 rm -rf *
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../../External/`uname -s`/ -DCMAKE_INSTALL_RPATH=../../../External/`uname -s`/ -DBUILD_EXAMPLES=OFF -DENABLE_EXTERNAL_LIBS=OFF -DBUILD_REGTEST=OFF ../../libsndfile
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../../External/`uname -s`/ -DCMAKE_INSTALL_RPATH=../../../External/`uname -s`/ -DENABLE_MPEG=OFF -DBUILD_EXAMPLES=OFF -DENABLE_EXTERNAL_LIBS=OFF -DBUILD_REGTEST=OFF ../../libsndfile
 cmake --build . --config Debug --target install
 popd
